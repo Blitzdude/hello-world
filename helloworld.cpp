ab9263263501f27571a15d61ee908f51c819f447
Joel Känsälä <joelkansala@gmail.com>
2017-01-21 13:19:50 +0200

    changes done v. 1.0.000012

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 int main()
 {
-  cout << "Hello World!" << endl;
-  return 0;
+	int x;
+	x = (3 + 3 - 1)  * 8;
+	cout << "x is equal to " << x << endl;
+	cout << "Hello World!" << endl;
+	return 0;
 }
