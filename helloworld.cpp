a9b1eedf8752d7a88eaaa12797d6d0528ffd42c6
Blitzdude <joelkansala@gmail.com>
2017-01-30 13:52:24 +0200

    Added gitignore and files for visual studio. also some use case diagrams

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+
 int main()
 {
-	int x;
-	x = (3 + 3 - 1)  * 8;
-	cout << "x is equal to " << x << endl;
-	cout << "Hello World!" << endl;
+	string name;
+	string address;
+	int age;
+
+	cout << "Hello world!\n";
+	cout << "What is your name"; 
+	cin >> name;
+	cout << "How old are you\n";
+	cin >> age;
+	cin.ignore();
+	cout << "What is your address 'wink wink'?\n";
+	getline(cin, address);
+
+	cout << "name: " << name << ", age: " << age << endl;
+	cout << "address: " << address;
+
+	
 	return 0;
 }
